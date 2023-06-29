2a657c8c07678bbf644dfe85d896e9cc73bb3dc5
pfviktor <viktocode@gmail.com>
2023-06-29 10:18:46 +0200

    calculator test implementation

--- src/test/java/com/codecool/marsexploration/calculators/service/CoordinateCalculatorImplTest.java
+++ src/test/java/com/codecool/marsexploration/calculators/service/CoordinateCalculatorImplTest.java
@@ -1,4 +1,69 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.codecool.marsexploration.calculators.service;
+
+import com.codecool.marsexploration.calculators.model.Coordinate;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class CoordinateCalculatorImplTest {
-  
+
+
+    @Test
+    public void testGetRandomCoordinate() {
+
+        CoordinateCalculatorImpl coordinateCalculator = new CoordinateCalculatorImpl();
+        int dimension = 10;
+        Coordinate coordinate = coordinateCalculator.getRandomCoordinate(dimension);
+        int x = coordinate.x();
+        int y = coordinate.y();
+        assertNotNull(coordinate);
+        assertTrue(x >= 0 && x < dimension);
+        assertTrue(y >= 0 && y < dimension);
+
+    }
+
+    @Test
+    public void testGetAdjacentCoordinatesZeroDimension() {
+
+        CoordinateCalculatorImpl coordinateCalculator = new CoordinateCalculatorImpl();
+
+            List<Coordinate> listOfCoordinates = new ArrayList<>();
+
+        Coordinate coordinate = new Coordinate(3, 4);
+
+        int dimension = 0;
+
+        listOfCoordinates.add(coordinate);
+
+        Iterable<Coordinate> results =  coordinateCalculator.getAdjacentCoordinates(coordinate, dimension);
+        Assertions.assertEquals(listOfCoordinates, results);
+
+
+    }
+
+    @Test
+    public void testGetAdjacentCoordinatesWithDimensionOne() {
+
+        CoordinateCalculatorImpl coordinateCalculator = new CoordinateCalculatorImpl();
+
+        List<Coordinate> listOfCoordinates = new ArrayList<>();
+
+        Coordinate coordinate = new Coordinate(3, 4);
+
+        int dimension = 1;
+
+        listOfCoordinates.add(coordinate);
+
+        Iterable<Coordinate> results =  coordinateCalculator.getAdjacentCoordinates(coordinate, dimension);
+        Assertions.assertEquals(listOfCoordinates, results);
+
+
+    }
+
 }
--- src/test/java/com/codecool/marsexploration/calculators/service/DimensionCalculatorImplTest.java
+++ src/test/java/com/codecool/marsexploration/calculators/service/DimensionCalculatorImplTest.java
@@ -1,4 +1,36 @@
+package com.codecool.marsexploration.calculators.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class DimensionCalculatorImplTest {
-  
+
+    private DimensionCalculatorImpl dimensionCalculator;
+
+    @BeforeEach
+    public void setUp() {
+        dimensionCalculator = new DimensionCalculatorImpl();
+    }
+
+    @Test
+
+    public void testCalculateDimension() {
+
+        int result1 = dimensionCalculator.calculateDimension(16, 0);
+        assertEquals(4, result1);
+
+        int result2 = dimensionCalculator.calculateDimension(25, -3);
+        assertEquals(2, result2);
+
+        int result3 = dimensionCalculator.calculateDimension(9, 2);
+        assertEquals(5, result3);
+
+
+    }
+
+
+
+
 }
